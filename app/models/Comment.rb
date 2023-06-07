f5b2a48bc580a70259383ebf027ceaf8425bc6db
mossa <martin.ossa.8@gmail.com>
2023-06-07 18:19:43 +0200

    Fix styling issues

--- app/models/Comment.rb
+++ app/models/Comment.rb
@@ -1,14 +1,13 @@
 class Comment < ApplicationRecord
-    belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-    belongs_to :post
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :post
 
-    after_create :update_post_comments_counter
-    after_destroy :update_post_comments_counter
-  
-    private
-  
-    def update_post_comments_counter
-      post.update(comments_counter: post.comments.count)
-    end
+  after_create :update_post_comments_counter
+  after_destroy :update_post_comments_counter
+
+  private
+
+  def update_post_comments_counter
+    post.update(comments_counter: post.comments.count)
+  end
 end
-  
