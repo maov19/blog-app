2425d5f8abb6b41712e9538f0996940caf46a00e
mossa <martin.ossa.8@gmail.com>
2023-06-09 13:32:03 +0200

    Add comment tests

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  describe 'associations' do
+    it 'belongs to an author' do
+      association = described_class.reflect_on_association(:author)
+      expect(association.macro).to eq(:belongs_to)
+      expect(association.options[:class_name]).to eq('User')
+      expect(association.options[:foreign_key]).to eq('author_id')
+    end
+  end
+
+  describe 'callbacks' do
+    let!(:user) { User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')}
+    let!(:post) { Post.new(author: user, title: 'First Post', text: 'Hello World') }
+    let!(:comment) { Comment.new(post: post, author: user, text: 'Great post!') }
+  
+    it 'updates post comments counter after create' do
+      expect {
+        comment.save
+        post.reload
+      }.to change(post, :comments_counter).by(1)
+    end
+  
+    it 'updates post comments counter after destroy' do
+      comment.save
+      expect {
+        comment.destroy
+        post.reload
+      }.to change(post, :comments_counter).by(-1)
+    end
+  end
+end
