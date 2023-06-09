8021194c410268e3b39b1a15dc0d8450213f26dc
mossa <martin.ossa.8@gmail.com>
2023-06-09 13:36:27 +0200

    Fix styling issues

--- db/migrate/20230606150255_update_comments.rb
+++ db/migrate/20230606150255_update_comments.rb
@@ -1,7 +1,7 @@
 class UpdateComments < ActiveRecord::Migration[7.0]
   def change
-    remove_column :comments, :author_id
-    remove_column :comments, :post_id
+    # remove_column :comments, :author_id
+    # remove_column :comments, :post_id
 
     add_reference :comments, :author, foreign_key: { to_table: :users }
     add_reference :comments, :post, foreign_key: true
--- db/migrate/20230606150350_update_likes.rb
+++ db/migrate/20230606150350_update_likes.rb
@@ -1,7 +1,7 @@
 class UpdateLikes < ActiveRecord::Migration[7.0]
   def change
-    remove_column :likes, :author_id
-    remove_column :likes, :post_id
+    # remove_column :likes, :author_id
+    # remove_column :likes, :post_id
 
     add_reference :likes, :author, foreign_key: { to_table: :users }
     add_reference :likes, :post, foreign_key: true
