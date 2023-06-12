d06a13c98e60c38cbfe1a8e4ac47fc8bf1d590b3
mossa <martin.ossa.8@gmail.com>
2023-06-12 13:27:50 +0200

    Add controllers for posts and users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  def index
+    # Placeholder for displaying all user names
+  end
+
+  def show
+    # Placeholder for displaying user, bio, posts, comment counter, and likes counter
+  end
+
+  def posts
+    # Placeholder for displaying user, posts, comments, comment counter, and likes counter
+  end
+
+  def show_post
+    # Placeholder for displaying post, comments, comment counter, and likes counter
+  end
+end
