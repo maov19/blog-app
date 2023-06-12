db20f10f5f32172a868e6cd08dda19c7f720763c
mossa <martin.ossa.8@gmail.com>
2023-06-12 14:29:33 +0200

    Fix routes code

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,6 @@
 Rails.application.routes.draw do
-  # Route for displaying all posts for a specific user
-  get 'users/:user_id/posts', to: 'posts#index', as: 'user_posts'
-
-  # Route for displaying a specific post and its comments
-  get 'users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
-
-  # Route for displaying all user names
-  get 'users', to: 'users#index'
-
-  # Route for displaying a specific user and their posts
-  get 'users/:id', to: 'users#show', as: 'user'
+  resources :users, only: [:index, :show] do 
+    resources :posts, only: [:index, :show] 
+  end
 end
 
