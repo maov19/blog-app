8cc92f014c5d85d758df62f0e8a5084e097d85d3
mossa <martin.ossa.8@gmail.com>
2023-06-21 12:50:29 +0200

    Update user and migrate to db

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'users/registrations', 
+    sessions: 'users/sessions'
+  }
+
   root 'users#index'
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show] do
