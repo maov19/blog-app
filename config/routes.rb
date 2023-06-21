25c6b986dbdcdd4ede527123856cfb3a33ed02db
edubew <edubew98@gmail.com>
2023-06-21 21:42:09 +0300

    update comment delete routes

--- config/routes.rb
+++ config/routes.rb
@@ -15,13 +15,14 @@ Rails.application.routes.draw do
   root 'users#index'
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show] do
-      resources :comments, only: [:new, :create]
+      resources :comments, only: [:new, :create, :destroy]
       resources :likes, only: [:create]
     end
   end
 
-  resources :posts, only: [:new, :create, :destroy] # Move the delete route here
-
-  delete '/users/:user_id/posts/:id', to: 'posts#destroy', as: 'destroy_user_post' # Use a different name for the delete route
+  resources :posts, only: [:new, :create, :destroy]
+  resources :comments, only: [:destroy], as: 'destroy_comment_post'
+  
+  delete '/users/:user_id/posts/:id', to: 'posts#destroy', as: 'destroy_user_post' 
 
 end
