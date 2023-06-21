87662e8b8399dc844b9670cd93a53169319303d3
mossa <martin.ossa.8@gmail.com>
2023-06-21 18:54:43 +0200

    fix styling issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # has_secure_password
   # Include default devise modules. Others available are:
-  
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   has_many :posts, foreign_key: 'author_id', dependent: :destroy
