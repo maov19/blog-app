30dfbec4146edeabcc0b48944772edd9d33dd1d1
edubew <edubew98@gmail.com>
2023-06-23 15:17:31 +0300

    add api authentication to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
-end
+    skip_before_action :verify_authenticity_token
+  
+    private
+  
+    def authenticate_api_request
+      auth_token = request.headers['x-api-key']
+      @current_user = User.find_by(auth_token:)
+  
+      return if @current_user
+  
+      render json: { error: 'Invalid authentication token' }, status: :unauthorized
+    end
+  end
