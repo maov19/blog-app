220ea4356e05d037f8b059a1b0c711d1a89fbb17
edubew <edubew98@gmail.com>
2023-06-23 14:33:34 +0300

    Implement all the requested test changes

--- spec/features/posts_show_spec.rb
+++ spec/features/posts_show_spec.rb
@@ -3,8 +3,11 @@ require 'rails_helper'
 RSpec.feature 'User Index Page', type: :feature do
   before do
     @user1 = User.create(name: 'Sam', photo: 'https://icons.iconarchive.com/icons/iconsmind/outline/256/User-icon.png',
-                         bio: 'Teacher from Poland.', posts_counter: 0)
-    visit users_path
+      bio: 'Teacher from Poland.', posts_counter: 0)
+    @post = Post.create(author: @user1, title: 'Test Post', text: 'This is my first test post', comments_counter: 0,
+      likes_counter: 0)
+    @comment1 = Comment.create(post: @post, author: @user1, text: 'Hi Tom!, Nice comment')
+      visit user_posts_path(@user1)
   end
 
   scenario 'I can see the username, profile picture, and number of posts for each user' do
