a80f62af6048f77184fb96bcd2916a941bd1618f
edubew <edubew98@gmail.com>
2023-06-23 15:15:06 +0300

    add api for users

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,25 +1,8 @@
-class Api::UsersController < ApplicationController
+class Api::UsersController < ApplicationController 
     def index
       @users = User.all
       respond_to do |format|
         format.json { render json: @users, status: :ok }
-      end 
-    end
-
-    def show
-      # Your code to fetch and return a specific user
-    end
-  
-    def create
-      # Your code to create a new user
-    end
-  
-    def update
-      # Your code to update a user
-    end
-  
-    def destroy
-      # Your code to delete a user
+      end
     end
   end
-  
